788d1388e8ea3cd55f82e873948cfaa4ab9828a8
AdMarom <adenamarom@gmail.com>
2023-03-15 13:35:57 -0700

    added css styleing

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 function startButtonFunction() {
-    // Declare document body variable
-var body = document.body;
-var questionsDiv = document.querySelector(".quiz-questions");
+
+        // Declare document body variable
+        var body = document.body;
+        var questionsDiv = document.querySelector(".quiz-questions");
 
 
-    var startButton = document.createElement("button");
-    startButton.textContent = "Start Quiz";
-    questionsDiv.appendChild(startButton);
+        var startButton = document.createElement("button");
+        startButton.textContent = "Start Quiz";
+        questionsDiv.appendChild(startButton);
 
-startButton.addEventListener("click", function() {
-    document.querySelector(".quiz-questions").innerHTML = ""
-    var timeElement = document.querySelector(".time");
-    var secondsLeft = 60;
-    // Start Timer
-    var timerInterval = setInterval(function(){
+        startButton.addEventListener("click", function() {
+        document.querySelector(".quiz-questions").innerHTML = ""
+        var timeElement = document.querySelector(".time");
+        var secondsLeft = 60;
+
+        // Start Timer
+        var timerInterval = setInterval(function(){
         secondsLeft--;
         timeElement.textContent = "Time Left: " + secondsLeft + " Seconds";
 
@@ -21,64 +23,57 @@ startButton.addEventListener("click", function() {
             clearInterval(timerInterval);
         }
 
-    }, 1000);
+        }, 1000);
+        firstQuestion();
 
 
+    function firstQuestion() {
 
-    // Append Quiz Title
-    var quizTitle = document.createElement("h1");
-    quizTitle.textContent = "JavaScript Quiz";
-    body.appendChild(quizTitle);
-    firstQuestion();
+        // Append First Question
+        questionsDiv.textContent = "lsfkabavvnwepionj awmpaivomwpoivno  eiowvnsobcnwioj ioabavpobaenw ";
+        body.appendChild(questionsDiv);
+        // Append Buttons
+        var answerA = document.createElement("button")
+        answerA.textContent = "Answer A ";
+        questionsDiv.appendChild(answerA);
+        var answerB = document.createElement("button")
+        answerB.textContent = "Answer B";
+        questionsDiv.appendChild(answerB);
+        var answerC = document.createElement("button")
+        answerC.textContent = "Answer C";
+        questionsDiv.appendChild(answerC);
 
-function firstQuestion() {
-
-    // Append First Question
-    questionsDiv.textContent = "lsfkabavvnwepionj awmpaivomwpoivno  eiowvnsobcnwioj ioabavpobaenw ";
-    body.appendChild(questionsDiv);
-    // Append Buttons
-    var answerA = document.createElement("button")
-    answerA.textContent = "Answer A ";
-    questionsDiv.appendChild(answerA);
-    var answerB = document.createElement("button")
-    answerB.textContent = "Answer B";
-    questionsDiv.appendChild(answerB);
-    var answerC = document.createElement("button")
-    answerC.textContent = "Answer C";
-    questionsDiv.appendChild(answerC);
-
-    //Subtract 10 seconds if button selected & start question 2
-    answerA.addEventListener("click", function(){
+        //Subtract 10 seconds if button selected & start question 2
+        answerA.addEventListener("click", function(){
         secondsLeft = secondsLeft - 10;
         timeElement.textContent = "Time Left: " + secondsLeft + " Seconds";
         secondQuestion()
-    })
+        })
 
-    //Subtract 10 seconds if button selected & start question 2
-    answerB.addEventListener("click", function(){
+        //Subtract 10 seconds if button selected & start question 2
+        answerB.addEventListener("click", function(){
         secondsLeft = secondsLeft - 10;
         timeElement.textContent = "Time Left: " + secondsLeft + " Seconds";
         secondQuestion()
-    })
+        })
 
-    //Add 10 points to score and start question 2
-    answerC.addEventListener("click", function(){
+        //Add 10 points to score and start question 2
+        answerC.addEventListener("click", function(){
         secondQuestion()
-    })
+        })
+
+    };
+
+
+    function secondQuestion() {
+        document.querySelector(".quiz-questions").innerHTML = ""
+        console.log("question 2")
+
+        // Append Second Question
+        questionsDiv.textContent = "new question";
+        body.appendChild(questionsDiv);
+    }
+    });
 
 };
-
-function secondQuestion() {
-    document.querySelector(".quiz-questions").innerHTML = ""
-    console.log("question 2")
-
-    // Append First Question
-    questionsDiv.textContent = "new question";
-    body.appendChild(questionsDiv);
-}
-
-
-
-});
-}
 startButtonFunction()
